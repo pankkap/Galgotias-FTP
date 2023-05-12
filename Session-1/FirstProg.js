972bd6a99c0fdd799373029124d830ac1366be5c
pankkap <pankkap@gmail.com>
2023-05-12 09:52:05 +0530

    Session-2

---
+++ Session-1/FirstProg.js
@@ -0,0 +1,15 @@
+console.log("This is First Day of Trainin")
+
+for(var i=0;i<10;i++)
+{
+    console.log(i)
+}
+
+
+function sayhello(name)
+{
+    return "hello" + name
+}
+
+
+console.log( sayhello("Galgotias"))
